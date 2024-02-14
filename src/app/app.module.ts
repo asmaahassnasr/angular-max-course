245a4bb4353aa24d12cd424d8411dcdb713778b6
Asmaa Hassan <asmaa14has@gmail.com>
2024-02-14 23:03:55 +0200

    Assignment: DataBinding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { WarningComponent } from './warning/warning.component';
 import { SuccessComponent } from './success/success.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
 
 @NgModule({
   declarations: [
-    AppComponent, WarningComponent, SuccessComponent
+    AppComponent, WarningComponent, SuccessComponent, DataBindingComponent
   ],
   imports: [
     BrowserModule, FormsModule
