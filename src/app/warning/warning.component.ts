7e2467b4872a62e5b72ba0ad2294bf089a1192a2
Asmaa Hassan <asmaa14has@gmail.com>
2024-02-14 18:30:19 +0200

    Assignment : generating components

---
+++ src/app/warning/warning.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector:'app-warning',
+  template:`
+    <p class="warning"> Warning , You are in danguer ! </p>
+  `,
+  styleUrls: ['./success.component.css']
+})
+export class WarningComponent{
+
+}
